c4a482f1eb51751739717c2d2d5be277719791b8
Adam Finch <atyreefinch@gmail.com>
2019-08-01 03:15:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,16 +1,16 @@
-def introduction (name) 
+def introduction(name) 
   puts "Hi, my name is #{name}."
 end
   
-def indroduction_with_language (name,language) 
+def indroduction_with_language(name, language) 
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def indroduction_with_language_optional (name,language="Ruby") 
+def indroduction_with_language_optional(name,language="Ruby") 
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
   
-def indroduction_with_language_optional (name,language*) 
+def indroduction_with_language_optional(name,language*) 
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
   
